063e8c358f6f1fc862a5d2464f9368d91475238b
ktzxs <lucasalvesdacosta070@gmail.com>
2026-03-02 17:52:55 -0300

    criado tratamento de erros

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,8 +1,30 @@
 import { Router } from 'express';
-import { prisma } from '../libs/prisma';
+import { createUser, createUsers } from '../service/user';
 
 export const mainRouter = Router();
 
 mainRouter.get('/ping', (req, res) => {
     res.json({ pong: true });
+});
+
+mainRouter.get('/test', (req, res) => {
+    res.json({ testando: true });
+});
+
+mainRouter.post('/user', async (req, res) => { 
+    //validar os dados de entrada
+    const user = await createUser ({
+        name: 'Lucas Alves',
+        email: 'lucasalves@gamil.com'
+    })
+    if(user) {
+        res.status(201).json({ user })
+    } else {
+        res.status(400).json({ error: 'Email already exists'})
+    }
+});
+
+mainRouter.post('/users', async (req, res) => {
+    const result = await createUsers([]);
+    res.status(201).json({ ok: true });
 });
---
+++ src/service/user.ts
@@ -0,0 +1,32 @@
+import { prisma } from '../libs/prisma';
+import { Prisma } from '@prisma/client';
+
+type CreateUserProps = {
+    name: string;
+    email: string;
+}
+
+export const createUser = async (data: Prisma.UserCreateInput) => {
+    try {
+        const user = await prisma.user.create({ data })
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === 'P2002') {
+                console.error('Email already exists');
+                return false;
+            }
+        }
+        console.error('Error creating user:', error);
+        return false;
+    }
+}
+
+export const createUsers = async (users: Prisma.UserCreateInput[]) => {
+    const result = await prisma.user.createMany({
+        data: [
+            {name: 'João', email: 'joao@exemple.com'},
+            {name: 'Ana', email: 'ana@exemple'},
+            {name: 'Lucap', email: 'lucap@exemple'}
+        ]
+    });
+}
